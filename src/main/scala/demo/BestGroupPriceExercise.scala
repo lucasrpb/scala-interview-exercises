dff942fd9f26323deb0ad2d0d79cf926c35e7d9b
lucasrpb <Ghb7140#@!>
2025-09-30 21:38:47 -0300

    Added more test cases.

--- src/main/scala/demo/BestGroupPriceExercise.scala
+++ src/main/scala/demo/BestGroupPriceExercise.scala
@@ -2,6 +2,19 @@ package demo
 
 object BestGroupPriceExercise {
 
+  class RatesCannotBeEmptyException extends RuntimeException("Rates cannot be empty!")
+  class RateCannotBelongToMoreThanOneRateGroupException(rates: Seq[String])
+    extends RuntimeException(s"One rate cannot belong to more than one rate group: ${rates}")
+
+  class NegativePricesException(prices: Seq[CabinPrice])
+    extends RuntimeException(s"Cabin prices cannot be negative: ${prices}")
+
+  class ConflictingPricesException(prices: Map[(String, String), Seq[CabinPrice]])
+    extends RuntimeException(s"Conflicting prices: ${prices}")
+
+  class NonExistingRateCodeException(rates: Seq[String])
+    extends RuntimeException(s"Non existing rate codes: ${rates}")
+
   case class Rate(rateCode:
                   String, rateGroup: String)
   case class CabinPrice(cabinCode: String,
@@ -14,12 +27,37 @@ object BestGroupPriceExercise {
 
   def getBestGroupPrices(rates: Seq[Rate],
                          prices: Seq[CabinPrice]): Seq[BestGroupPrice] = {
-    val counter = rates.filter(r => rates.count(s => r.rateCode.compareTo(s.rateCode) == 0) > 1)
-    if(!counter.isEmpty){
-      throw new RuntimeException(s"One rate cannot belong to more than one rate group: ${counter.map(_.rateCode)}")
+    if(rates.isEmpty){
+      throw new RatesCannotBeEmptyException()
     }
 
     val ratesMap = rates.map{r => r.rateCode -> r}.toMap
+
+    val nonExistingRateCodes = prices.filterNot(p => ratesMap.isDefinedAt(p.rateCode))
+
+    if(!nonExistingRateCodes.isEmpty){
+      throw new NonExistingRateCodeException(nonExistingRateCodes.map(_.rateCode))
+    }
+
+    val rateCounter = rates.filter(r => rates.count(s => r.rateCode.compareTo(s.rateCode) == 0) > 1)
+    if(!rateCounter.isEmpty){
+      throw new RateCannotBelongToMoreThanOneRateGroupException(rateCounter.map(_.rateCode))
+    }
+
+    val negativeCounter = prices.filter(p => p.price < 0)
+
+    if(!negativeCounter.isEmpty){
+      throw new NegativePricesException(negativeCounter)
+    }
+
+    val conflictingPrices = prices.groupBy(p => p.rateCode -> p.cabinCode).map { case (tuple, list) =>
+      tuple -> list.distinctBy(_.price)
+    }.filter(_._2.length > 1)
+
+    if(!conflictingPrices.isEmpty){
+      throw new ConflictingPricesException(conflictingPrices)
+    }
+
 
     // Group prices by cabin code to find the list of rates and its rate groups (like a join between rate and cabin price)
     prices.groupBy(_.cabinCode).flatMap { case (cabinCode, listOfRates) =>
--- src/test/scala/demo/BestGroupPriceExerciseTest.scala
+++ src/test/scala/demo/BestGroupPriceExerciseTest.scala
@@ -55,7 +55,101 @@ class BestGroupPriceExerciseTest  extends AnyFlatSpec {
       CabinPrice("CB", "S2", 270.00)
     )
 
-    assertThrows[RuntimeException](getBestGroupPrices(rates, prices))
+    assertThrows[RateCannotBelongToMoreThanOneRateGroupException](getBestGroupPrices(rates, prices))
+
+  }
+
+  "it" should " not have rates empty" in {
+
+    val rates = Seq.empty[Rate]
+
+    val prices = Seq(
+      CabinPrice("CA", "M1", 200.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00),
+      CabinPrice("CA", "S2", 260.00),
+      CabinPrice("CB", "M1", 230.00),
+      CabinPrice("CB", "M2", 260.00),
+      CabinPrice("CB", "S1", 245.00),
+      CabinPrice("CB", "S2", 270.00)
+    )
+
+    assertThrows[RatesCannotBeEmptyException](getBestGroupPrices(rates, prices))
+
+  }
+
+  "it" should " throw an exception if prices are negative " in {
+
+    val rates = Seq(
+      Rate("M1", "Military"),
+      Rate("M2", "Military"),
+      Rate("S1", "Senior"),
+      Rate("S2", "Senior")
+    )
+
+    val prices = Seq(
+      CabinPrice("CA", "M1", -200.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00),
+      CabinPrice("CA", "S2", 260.00),
+      CabinPrice("CB", "M1", 230.00),
+      CabinPrice("CB", "M2", 260.00),
+      CabinPrice("CB", "S1", 245.00),
+      CabinPrice("CB", "S2", 270.00)
+    )
+
+    assertThrows[NegativePricesException](getBestGroupPrices(rates, prices))
+
+  }
+
+  "it" should " not have different prices (conflicting ones) for a particular (cabin code, rate code) combination " in {
+
+    val rates = Seq(
+      Rate("M1", "Military"),
+      Rate("M2", "Military"),
+      Rate("S1", "Senior"),
+      Rate("S2", "Senior")
+    )
+
+    val prices = Seq(
+      CabinPrice("CA", "M1", 200.00),
+      CabinPrice("CA", "M1", 100.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00),
+      CabinPrice("CA", "S2", 260.00),
+      CabinPrice("CB", "M1", 230.00),
+      CabinPrice("CB", "M2", 260.00),
+      CabinPrice("CB", "S1", 245.00),
+      CabinPrice("CB", "S2", 270.00)
+    )
+
+    assertThrows[ConflictingPricesException](getBestGroupPrices(rates, prices))
+
+  }
+
+  "it" should " not have cabin prices referring a non existing rate code " in {
+
+    val rates = Seq(
+      Rate("M1", "Military"),
+      Rate("M2", "Military"),
+      Rate("S1", "Senior"),
+      Rate("S2", "Senior")
+    )
+
+    val prices = Seq(
+      CabinPrice("CA", "M7", 300.00),
+      CabinPrice("CA", "M1", 200.00),
+      CabinPrice("CA", "M1", 100.00),
+      CabinPrice("CA", "M2", 250.00),
+      CabinPrice("CA", "S1", 225.00),
+      CabinPrice("CA", "S2", 260.00),
+      CabinPrice("CB", "M1", 230.00),
+      CabinPrice("CB", "M2", 260.00),
+      CabinPrice("CB", "S1", 245.00),
+      CabinPrice("CB", "S2", 270.00)
+    )
+
+    assertThrows[NonExistingRateCodeException](getBestGroupPrices(rates, prices))
 
   }
 
